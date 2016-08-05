9e29b0e9d0da4f6e51284d5d2f6f8be37a47cf98
vodek@bitbucket.org <vodek@bitbucket.org>
2016-08-05 11:14:29 +0100

    spotify service implemented

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {NavbarTopComponent} from './components/navbar-top/navbar-top.component';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {SpotifyService} from './services/spotify.service';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    directives: [ROUTER_DIRECTIVES, NavbarTopComponent]
+    directives: [ROUTER_DIRECTIVES, NavbarTopComponent],
+    providers: [SpotifyService, HTTP_PROVIDERS]
 })
 export class AppComponent { }
--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
     moduleId: module.id,
     selector: 'search',
     templateUrl: 'search.component.html'
 })
-export class SearchComponent { }
+export class SearchComponent {
+    searchStr:string;
+
+    constructor(private _spotifyService: SpotifyService){
+
+    }
+
+    searchMusic(){
+        // console.log(this.searchStr);
+        this._spotifyService.searchMusic(this.searchStr)
+        .subscribe(res=>{
+            console.log(res);
+        })
+    }
+ }
